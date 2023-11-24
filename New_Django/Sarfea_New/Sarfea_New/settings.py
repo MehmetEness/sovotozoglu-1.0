b7b497c26fa2f8276e67c582b01ebd44c62eeae2
MehmetEness <95481557+MehmetEness@users.noreply.github.com>
2023-11-24 04:56:28 +0300

    Update settings.py
    Setting satır 131-132-132 e alan adı tanımı yapıldı

--- New_Django/Sarfea_New/Sarfea_New/settings.py
+++ New_Django/Sarfea_New/Sarfea_New/settings.py
@@ -127,3 +127,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Alan adı tanımlama
+
+ALLOWED_HOSTS = ['sovotozoglu.com', '3.90.217.139', 'sovotozoglu.net']
+

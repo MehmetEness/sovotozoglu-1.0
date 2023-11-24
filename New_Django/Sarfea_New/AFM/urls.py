5be4811e1508f76c7c9c5e5f544b380048c4ea6f
MehmetEness <95481557+MehmetEness@users.noreply.github.com>
2023-11-24 04:27:58 +0300

    sovotozoglu-1.0

---
+++ New_Django/Sarfea_New/AFM/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+#http://127.0.0.1:8000/      =>Home Page
+#http://127.0.0.1:8000/home     =>Home Page
+
+
+
+
+urlpatterns = [
+    #path("admin/", admin.site.urls),
+    path("", views.home),
+    path("home", views.home, name='home'),
+    path("projects/", views.projects, name='projects'),
+    path('project_details/<str:project_name>/', views.project_details, name='project_details'),
+    path("realized_cost/<str:project_name>/", views.realized_cost, name='realized_cost'),
+    path('income_details/<str:project_name>/', views.income_details, name='income_details'),
+    path("client/", views.client, name='client'),
+    path("supplier/", views.supplier, name='supplier'),
+]
